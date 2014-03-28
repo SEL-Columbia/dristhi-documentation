87998db5358fab3fb57b37e5b374506945ef8033
Kiran <kirang20@gmail.com>
2014-03-28 19:50:43 +0530

    Kiran | #934 | Changed root_url to dristhi-documentation to get gh-pages deployment ready.

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/dristhi-documentation/"
+http_images_path = "/dristhi-documentation/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/dristhi-documentation/fonts"
+css_dir = "public/dristhi-documentation/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
